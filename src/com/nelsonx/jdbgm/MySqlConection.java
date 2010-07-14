0e9d909105427d53db123ca0ec167d65ace37e67
nerones <neac03@gmail.com>
2010-07-14 05:42:06 +0000

    iniciando

---
+++ src/com/nelsonx/jdbgm/MySqlConection.java
@@ -0,0 +1,19 @@
+package com.nelsonx.jdbgm;
+
+import com.nelsonx.jdbgm.core.GenericConection;
+
+public class MySqlConection implements GenericConection {
+
+	@Override
+	public void getConection() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void closeConection() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
